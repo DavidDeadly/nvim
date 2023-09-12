4ca3dee72ddfff8c9f2864aacdc73eb0f454c2d2
DavidDeadly <jdrueda513@gmail.com>
2023-09-12 00:03:37 -0500

    Add lazy git support

--- lua/daviddeadly/plugins/git.lua
+++ lua/daviddeadly/plugins/git.lua
@@ -1,6 +1,15 @@
 return {
-  'tpope/vim-fugitive',
-  keys = {
-    { '<leader>gf', vim.cmd.Git, desc = 'Git fugitive' }
-  }
+  {
+    'tpope/vim-fugitive',
+    keys = {
+      { '<leader>gf', vim.cmd.Git, desc = 'Git fugitive' }
+    }
+  },
+
+  {
+    "kdheepak/lazygit.nvim",
+    dependencies = {
+      "nvim-lua/plenary.nvim",
+    },
+  },
 }
