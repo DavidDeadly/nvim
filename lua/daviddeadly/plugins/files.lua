e8195770e713c6c9905d570619bb9c99c43b13e7
DavidDeadly <jdrueda513@gmail.com>
2023-09-15 13:03:32 -0500

    add mini files as a file system manager buffer based

--- lua/daviddeadly/plugins/files.lua
+++ lua/daviddeadly/plugins/files.lua
@@ -4,40 +4,60 @@ local width = 60
 local height = 20
 
 return {
-  'nvim-tree/nvim-tree.lua',
-  opts = {
-    sort_by = "case_sensitive",
-    view = {
-      float = {
-        enable = true,
-        open_win_config = {
-          border = "rounded",
-          relative = 'editor',
-          width = width,
-          height = height,
-          row = (gheight - height) * 0.4,
-          col = (gwidth - width) * 0.5,
-        },
+  {
+    'echasnovski/mini.files', version = '*',
+    dependencies = { 'nvim-tree/nvim-web-devicons', opt = true },
+    opts = {
+      options = {
+        permanent_delete = false,
+        use_as_default_explorer = true,
       },
-    },
-    renderer = {
-      group_empty = true,
-    },
-    filters = {
-      custom = { "^.git$" }
-    },
-    actions = {
-      open_file = {
-        quit_on_open = true,
+
+      windows = {
+        preview = true
       }
     },
-    filesystem_watchers = {
-      enable = true
-    },
-    reload_on_bufenter = true
+    keys = {
+      { '<leader>m', '<cmd>:lua MiniFiles.open()<cr>', desc = 'Mini [E]xplorer' }
+    }
   },
-  keys = {
-    { '<leader>e', '<cmd>NvimTreeToggle<cr>', desc = 'Toggle file explorer' },
-    { '<leader>E', '<cmd>NvimTreeFindFile<cr>', desc = 'Find file in file explorer' },
+
+  {
+    'nvim-tree/nvim-tree.lua',
+    opts = {
+      sort_by = "case_sensitive",
+      view = {
+        float = {
+          enable = true,
+          open_win_config = {
+            border = "rounded",
+            relative = 'editor',
+            width = width,
+            height = height,
+            row = (gheight - height) * 0.4,
+            col = (gwidth - width) * 0.5,
+          },
+        },
+      },
+      renderer = {
+        group_empty = true,
+      },
+      filters = {
+        custom = { "^.git$" }
+      },
+      actions = {
+        open_file = {
+          quit_on_open = true,
+        }
+      },
+      filesystem_watchers = {
+        enable = true
+      },
+      reload_on_bufenter = true
+    },
+    keys = {
+      { '<leader>e', '<cmd>NvimTreeToggle<cr>', desc = 'Toggle file explorer' },
+      { '<leader>E', '<cmd>NvimTreeFindFile<cr>', desc = 'Find file in file explorer' },
+    }
   }
 }
