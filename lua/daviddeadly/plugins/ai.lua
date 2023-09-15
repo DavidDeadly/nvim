af5cb1c6d61e71913fe69ad5c9a923129b4b82b9
DavidDeadly <jdrueda513@gmail.com>
2023-09-15 00:37:01 -0500

    add copilot status to the status line

--- lua/daviddeadly/plugins/ai.lua
+++ lua/daviddeadly/plugins/ai.lua
@@ -4,6 +4,23 @@ return {
     event = 'VeryLazy',
     dependencies = {
       {
+        "jonahgoldwastaken/copilot-status.nvim",
+        lazy = true,
+        event = "BufReadPost",
+        opts = {
+          icons = {
+            idle = "-<",
+            error = "-<",
+            offline = "-<",
+            warning = "-<𥉉",
+            loading = "-<",
+          }
+        },
+        config = function(_, opts)
+          require("copilot_status").setup(opts)
+        end,
+      },
+      {
         "zbirenbaum/copilot.lua",
         cmd = "Copilot",
         build = ":Copilot auth",
--- lua/daviddeadly/plugins/status.lua
+++ lua/daviddeadly/plugins/status.lua
@@ -2,6 +2,7 @@ local lazy_status = require("lazy.status")
 
 return {
   'nvim-lualine/lualine.nvim',
+  event = 'VeryLazy',
   dependencies = { 'nvim-tree/nvim-web-devicons', opt = true },
   opts = {
     sections = {
@@ -19,10 +20,14 @@ return {
           cond = lazy_status.has_updates,
           color = { fg = '#7e9ce4' }
         },
+        {
+          function() return require("copilot_status").status_string() end,
+          cond = function() return require("copilot_status").enabled() end,
+        },
         'encoding',
         'fileformat',
         'filetype'
       }
     }
-  },
+  }
 }
