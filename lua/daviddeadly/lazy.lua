776bace849f3050af5d72af95503aaf85aad9af8
DavidDeadly <jdrueda513@gmail.com>
2023-09-17 15:37:46 -0500

    add fterm for terminal management

--- lua/daviddeadly/lazy.lua
+++ lua/daviddeadly/lazy.lua
@@ -25,28 +25,4 @@ require("lazy").setup("daviddeadly.plugins", {
   }
 })
 
-local lazy = require('lazy.util');
-
-local function floating_terminal()
-  lazy.float_term(nil, {
-    size = { width = 0.8, height = 0.8 }
-  })
-end
-
-local function lazy_git()
-  local hasLazyGit = vim.fn.executable('lazygit') == 1;
-
-  if hasLazyGit ~= true then
-    print("Please install LazyGit to use this command")
-    return
-  end
-
-  lazy.float_term('lazygit', {
-    size = { width = 0.8, height = 0.8 }
-  })
-end
-
 vim.keymap.set('n', '<leader>l', vim.cmd.Lazy, { desc = 'open lazy' })
-
-vim.keymap.set('n', '<leader>ñ', floating_terminal, { desc = 'Floating terminal' })
-vim.keymap.set('n', '<leader>gg', lazy_git, { desc = 'Lazygit' })
---
+++ lua/daviddeadly/plugins/terminal.lua
@@ -0,0 +1,28 @@
+local function lazy_git()
+  local fterm = require('FTerm')
+
+  return fterm:new({
+    cmd = "lazygit",
+    dimensions = {
+      height = 0.8,
+      width = 0.8
+    }
+  })
+end
+
+return {
+  'numToStr/FTerm.nvim',
+  opts = function()
+    local lazygit = lazy_git()
+    vim.api.nvim_create_user_command('LazyGit', function() lazygit:toggle() end, { bang = true })
+
+    return {}
+  end,
+  keys = {
+    { '<A-g><A-g>', '<CMD>LazyGit<CR>', desc = 'toggle LazyGit' },
+    { '<A-g><A-g>', '<C-\\><C-n><CMD>LazyGit<CR>', mode = 't', desc = 'toggle LazyGit' },
+    { '<A-ñ>', '<CMD>lua require("FTerm").toggle()<CR>', desc = 'toggle FTerminal' },
+    { '<A-ñ>', '<C-\\><C-n><CMD>lua require("FTerm").toggle()<CR>', mode = 't', desc = 'toggle FTerminal' },
+    { '<A-c>', '<C-\\><C-n><CMD>lua require("FTerm").exit()<CR>', mode = 't', desc = 'toggle FTerminal' },
+  }
+}
