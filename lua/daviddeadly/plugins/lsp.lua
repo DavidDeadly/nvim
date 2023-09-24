46ed27df20252b323627848b7065ad122a1b68bb
DavidDeadly <jdrueda513@gmail.com>
2023-09-23 23:26:40 -0500

    add lua formatter and, eslint_d & prettier_d

--- lua/daviddeadly/plugins/lsp.lua
+++ lua/daviddeadly/plugins/lsp.lua
@@ -1,36 +1,32 @@
+-- luacheck: globals vim
 return {
-  'neovim/nvim-lspconfig',
+  "neovim/nvim-lspconfig",
   event = { "BufReadPre", "BufNewFile" },
   dependencies = {
     -- Automatically install LSPs to stdpath for neovim
-    { 'williamboman/mason.nvim', config = true },
-    'williamboman/mason-lspconfig.nvim',
+    { "williamboman/mason.nvim", config = true },
+    "williamboman/mason-lspconfig.nvim",
 
-    'hrsh7th/cmp-nvim-lsp',
-    'hrsh7th/cmp-buffer',
-    'hrsh7th/cmp-path',
-    'hrsh7th/cmp-cmdline',
+    "hrsh7th/cmp-nvim-lsp",
+    "hrsh7th/cmp-buffer",
+    "hrsh7th/cmp-path",
+    "hrsh7th/cmp-cmdline",
 
     {
       "ray-x/lsp_signature.nvim",
       opts = {},
-      main = 'lsp_signature'
+      main = "lsp_signature",
     },
 
     {
-      'creativenull/efmls-configs-nvim',
-      version = 'v1.x.x', -- tag is optional, but recommended
-    },
-
-    {
-      "lukas-reineke/lsp-format.nvim",
-      opts = {}
+      "creativenull/efmls-configs-nvim",
+      version = "v1.x.x", -- tag is optional, but recommended
     },
 
     -- Useful status updates for LSP
     -- NOTE: `opts = {}` is the same as calling `require('fidget').setup({})`
-    { 'j-hui/fidget.nvim', tag = 'legacy', opts = {} },
-    { 'onsails/lspkind.nvim' },
+    { "j-hui/fidget.nvim",       tag = "legacy", opts = {} },
+    { "onsails/lspkind.nvim" },
 
     {
       "SmiteshP/nvim-navbuddy",
@@ -40,32 +36,31 @@ return {
           opts = {
             highlight = true,
             depth_limit = 5,
-            lsp = { auto_attach = true } 
-          }
+            lsp = { auto_attach = true },
+          },
         },
-        { "MunifTanjim/nui.nvim" }
+        { "MunifTanjim/nui.nvim" },
       },
       opts = { lsp = { auto_attach = true } },
       keys = {
-        { '<leader>nb', '<CMD>Navbuddy<CR>', desc = 'Navbuddy breadcrumbs' }
-      }
+        { "<leader>nb", "<CMD>Navbuddy<CR>", desc = "Navbuddy breadcrumbs" },
+      },
     },
 
     -- Additional lua configuration, makes nvim stuff amazing!
-    'folke/neodev.nvim',
+    "folke/neodev.nvim",
   },
 
   config = function()
     local on_attach = function(_, bufnr)
-
       local signature = require("lsp_signature")
       signature.on_attach({
         bind = true, -- This is mandatory, otherwise border config won't get registered.
-        toggle_key = '<C-k>',
+        toggle_key = "<C-k>",
         toggle_key_flip_floatwin_setting = true,
         noice = true,
         handler_opts = {
-          border = "rounded"
+          border = "rounded",
         },
       }, bufnr)
 
@@ -75,39 +70,50 @@ return {
 
       local nmap = function(keys, func, desc, modes)
         if desc then
-          desc = 'LSP: ' .. desc
+          desc = "LSP: " .. desc
         end
 
-        vim.keymap.set(modes or 'n', keys, func, { buffer = bufnr, desc = desc })
+        vim.keymap.set(modes or "n", keys, func, { buffer = bufnr, desc = desc })
       end
 
-      nmap('<leader>rn', vim.lsp.buf.rename, '[r]e[n]ame')
-      nmap('<leader>ca', vim.lsp.buf.code_action, '[c]ode [a]ction', { 'n', 'v' })
+      nmap("<leader>rn", vim.lsp.buf.rename, "[r]e[n]ame")
+      nmap("<leader>ca", vim.lsp.buf.code_action, "[c]ode [a]ction", { "n", "v" })
 
-      nmap('gd', vim.lsp.buf.definition, '[g]oto [d]efinition')
-      nmap('gr', require('telescope.builtin').lsp_references, '[g]oto [r]eferences')
-      nmap('gI', require('telescope.builtin').lsp_implementations, '[g]oto [I]mplementation')
-      nmap('<leader>D', vim.lsp.buf.type_definition, 'Type [D]efinition')
-      nmap('<leader>dS', require('telescope.builtin').lsp_document_symbols, '[d]ocument [s]ymbols')
-      nmap('<leader>ds', require("nvim-navbuddy").open, '[w]orspaces [s]ymbols')
-      nmap('<leader>ws', require('telescope.builtin').lsp_dynamic_workspace_symbols, '[w]orkspace [s]ymbols')
+      nmap("gd", vim.lsp.buf.definition, "[g]oto [d]efinition")
+      nmap("gr", require("telescope.builtin").lsp_references, "[g]oto [r]eferences")
+      nmap("gI", require("telescope.builtin").lsp_implementations, "[g]oto [I]mplementation")
+      nmap("<leader>D", vim.lsp.buf.type_definition, "Type [D]efinition")
+      nmap("<leader>dS", require("telescope.builtin").lsp_document_symbols, "[d]ocument [s]ymbols")
+      nmap("<leader>ds", require("nvim-navbuddy").open, "[w]orspaces [s]ymbols")
+      nmap("<leader>ws", require("telescope.builtin").lsp_dynamic_workspace_symbols, "[w]orkspace [s]ymbols")
 
       -- See `:help K` for why this keymap
-      nmap('K', vim.lsp.buf.hover, 'Hover Documentation')
-      nmap('<C-k>', signature.toggle_float_win, 'Toggle Signature Documentation')
+      nmap("K", vim.lsp.buf.hover, "Hover Documentation")
+      nmap("<C-k>", signature.toggle_float_win, "Toggle Signature Documentation")
 
-      nmap('<leader>vws', vim.lsp.buf.workspace_symbol, '[V]iew [W]orkspace [S]ymbols')
-      nmap('<leader>vd', function() vim.diagnostic.open_float() end, '[V]iew [D]iagnostic float')
-      nmap('[d', function() vim.diagnostic.goto_next() end, 'Next diagnostic')
-      nmap(']d', function() vim.diagnostic.goto_prev() end, 'Previous diagnostic')
+      nmap("<leader>vws", vim.lsp.buf.workspace_symbol, "[V]iew [W]orkspace [S]ymbols")
+      nmap("<leader>vd", function()
+        vim.diagnostic.open_float()
+      end, "[V]iew [D]iagnostic float")
+      nmap("[d", function()
+        vim.diagnostic.goto_next()
+      end, "Next diagnostic")
+      nmap("]d", function()
+        vim.diagnostic.goto_prev()
+      end, "Previous diagnostic")
       -- Lesser used LSP functionality
-      nmap('gD', vim.lsp.buf.declaration, '[g]oto [D]eclaration')
-      nmap('<leader>wa', vim.lsp.buf.add_workspace_folder, '[w]orkspace [a]dd Folder')
-      nmap('<leader>wr', vim.lsp.buf.remove_workspace_folder, '[w]orkspace [r]emove Folder')
-      nmap('<leader>wl', function()
+      nmap("gD", vim.lsp.buf.declaration, "[g]oto [D]eclaration")
+      nmap("<leader>wa", vim.lsp.buf.add_workspace_folder, "[w]orkspace [a]dd Folder")
+      nmap("<leader>wr", vim.lsp.buf.remove_workspace_folder, "[w]orkspace [r]emove Folder")
+      nmap("<leader>wl", function()
         print(vim.inspect(vim.lsp.buf.list_workspace_folders()))
-      end, '[w]orkspace [l]ist Folders')
+      end, "[w]orkspace [l]ist Folders")
 
+      nmap("<M-f>", "<CMD>Format<CR>", "Format file")
+      -- Create a command `:Format` local to the LSP buffer
+      vim.api.nvim_buf_create_user_command(bufnr, "Format", function(_)
+        vim.lsp.buf.format()
+      end, { desc = "Format current buffer with LSP" })
     end
 
     -- Enable the following language servers
@@ -118,25 +124,29 @@ return {
     --
     --  If you want to override the default filetypes that your language server will attach to you can
     --  define the property 'filetypes' to the map in question.
-    local eslint = require('efmls-configs.linters.eslint')
-    local prettier = require('efmls-configs.formatters.prettier')
-    local stylua = require('efmls-configs.formatters.stylua')
+    local eslint_d = require("efmls-configs.linters.eslint_d")
+    local prettier_d = require("efmls-configs.formatters.prettier_d")
+    local luacheck = require("efmls-configs.linters.luacheck")
+    local stylua = require("efmls-configs.formatters.stylua")
 
     local languages = {
-      typescript = { eslint, prettier },
-      javascript = { eslint, prettier },
-      jsx = { eslint, prettier },
-      tsx = { eslint, prettier },
-      lua = { stylua },
+      typescript = { eslint_d, prettier_d },
+      javascript = { eslint_d, prettier_d },
+      jsx = { eslint_d, prettier_d },
+      tsx = { eslint_d, prettier_d },
+      lua = { stylua, luacheck },
     }
 
     local servers = {
       pyright = {},
       tsserver = {},
-      html = { filetypes = { 'html', 'twig', 'hbs' } },
+      html = { filetypes = { "html", "twig", "hbs" } },
 
       lua_ls = {
         Lua = {
+          diagnostics = {
+            globals = { "vim" },
+          },
           workspace = { checkThirdParty = false },
           telemetry = { enable = false },
         },
@@ -145,38 +155,37 @@ return {
       efm = {
         filetypes = vim.tbl_keys(languages),
         settings = {
-          rootMarkers = { '.git/' },
+          rootMarkers = { ".git/" },
           languages = languages,
         },
         init_options = {
           documentFormatting = true,
           documentRangeFormatting = true,
           hover = true,
-          documentSymbol = false,
           codeAction = true,
-          completion = true
-        }
-      }
+          completion = true,
+        },
+      },
     }
 
     -- Setup neovim lua configuration
-    require('neodev').setup()
+    require("neodev").setup()
 
     -- nvim-cmp supports additional completion capabilities, so broadcast that to servers
     local capabilities = vim.lsp.protocol.make_client_capabilities()
-    capabilities = require('cmp_nvim_lsp').default_capabilities(capabilities)
+    capabilities = require("cmp_nvim_lsp").default_capabilities(capabilities)
 
     -- Ensure the servers above are installed
-    local mason_lspconfig = require 'mason-lspconfig'
+    local mason_lspconfig = require("mason-lspconfig")
 
-    mason_lspconfig.setup {
+    mason_lspconfig.setup({
       ensure_installed = vim.tbl_keys(servers),
-    }
+    })
 
-    mason_lspconfig.setup_handlers {
+    mason_lspconfig.setup_handlers({
       function(server_name)
-        if server_name == 'efm' then
-          require('lspconfig')[server_name].setup(vim.tbl_extend('force', servers[server_name], {
+        if server_name == "efm" then
+          require("lspconfig")[server_name].setup(vim.tbl_extend("force", servers[server_name], {
             on_attach = on_attach,
             capabilities = capabilities,
           }))
@@ -184,13 +193,13 @@ return {
           return
         end
 
-        require('lspconfig')[server_name].setup {
+        require("lspconfig")[server_name].setup({
           capabilities = capabilities,
           on_attach = on_attach,
           settings = servers[server_name],
           filetypes = (servers[server_name] or {}).filetypes,
-        }
-      end
-    }
-  end
+        })
+      end,
+    })
+  end,
 }
