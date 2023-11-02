ada91a19afc445ba5df7df1882dd9273c5844b2b
DavidDeadly <jdrueda513@gmail.com>
2023-11-02 09:59:27 -0500

    replace none-ls format & lint for conform and nvim lint respectively

--- lua/daviddeadly/plugins/none-ls.lua
+++ lua/daviddeadly/plugins/none-ls.lua
@@ -1,64 +1,103 @@
+-- luacheck: globals vim
 local eslint_filetypes = {
-  "javascript",
-  "javascriptreact",
-  "typescript",
-  "typescriptreact",
-  "vue",
-  "html"
+	"javascript",
+	"javascriptreact",
+	"typescript",
+	"typescriptreact",
+	"vue",
+	"html",
 }
 
 return {
-  "nvimtools/none-ls.nvim",
-  event = { "BufReadPre", "BufNewFile" },
-  dependencies = {
-    {
-      "davidmh/cspell.nvim"
-    }
-  },
-  opts = function()
-    local none_ls = require("null-ls")
-    local cspell = require("cspell")
+	{
+		"mfussenegger/nvim-lint",
+		event = { "BufReadPre", "BufNewFile" },
+		config = function()
+			vim.api.nvim_create_autocmd({ "BufWritePost" }, {
+				callback = function()
+					require("lint").try_lint()
+				end,
+			})
 
-    local diagnostics_conf = {
-      method = none_ls.methods.DIAGNOSTICS_ON_SAVE,
-    }
+			require("lint").linters_by_ft = {
+				javascript = { "eslint", "cspell" },
+				typescript = { "eslint", "cspell" },
+				javascriptreact = { "eslint", "cspell" },
+				typescriptreact = { "eslint", "cspell" },
+				html = { "eslint", "cspell" },
 
-    return {
-      sources = {
-        -- typescript
-        none_ls.builtins.formatting.prettierd,
-        -- none_ls.builtins.formatting.eslint_d,
-        -- none_ls.builtins.code_actions.eslint_d,
-        none_ls.builtins.formatting.eslint,
-        none_ls.builtins.code_actions.eslint.with({
-          filetypes = eslint_filetypes
-        }),
-        none_ls.builtins.diagnostics.eslint.with({
-          method = none_ls.methods.DIAGNOSTICS_ON_SAVE,
-          filetypes = eslint_filetypes
-        }),
+				lua = { "luacheck", "cspell" },
 
-        -- lua
-        none_ls.builtins.formatting.stylua,
-        none_ls.builtins.diagnostics.luacheck.with(diagnostics_conf),
+				python = { "flake8", "cspell" },
+			}
+		end,
+	},
 
-        -- python
-        none_ls.builtins.diagnostics.flake8,
-        none_ls.builtins.formatting.black,
+	{
 
-        -- spelling
-        none_ls.builtins.formatting.codespell,
-        none_ls.builtins.diagnostics.codespell.with(diagnostics_conf),
-        none_ls.builtins.diagnostics.misspell.with(diagnostics_conf),
-        cspell.diagnostics.with(diagnostics_conf),
-        cspell.code_actions,
-        none_ls.builtins.completion.spell,
+		"stevearc/conform.nvim",
+		event = { "BufReadPre", "BufNewFile" },
+		keys = {
+			{
+				"<M-f>",
+				function()
+					require("conform").format({
+						lsp_fallback = true,
+						async = false,
+						timeout_ms = 500,
+					})
+				end,
+				mode = { "n", "v" },
+				desc = "Format file or range (in visual mode) with conform",
+			},
+		},
+		opts = {
+			-- format_on_save = {
+			-- 	lsp_fallback = true,
+			-- 	async = false,
+			-- 	timeout_ms = 500,
+			-- },
+			formatters_by_ft = {
+				lua = { "stylua" },
 
-        -- general
-        none_ls.builtins.code_actions.gitsigns,
-        none_ls.builtins.completion.luasnip,
-        none_ls.builtins.hover.dictionary
-      }
-    }
-  end
+				python = { "isort", "black" },
+
+				javascript = { { "prettierd", "prettier" } },
+				typescript = { { "prettierd", "prettier" } },
+				javascriptreact = { { "prettierd", "prettier" } },
+				typescriptreact = { { "prettierd", "prettier" } },
+				html = { { "prettierd", "prettier" } },
+			},
+		},
+	},
+
+	{
+		"nvimtools/none-ls.nvim",
+		event = { "BufReadPre", "BufNewFile" },
+		dependencies = {
+			{
+				"davidmh/cspell.nvim",
+			},
+		},
+		opts = function()
+			local none_ls = require("null-ls")
+			local cspell = require("cspell")
+
+			return {
+				sources = {
+					-- typescript
+					-- none_ls.builtins.code_actions.eslint_d,
+					none_ls.builtins.code_actions.eslint.with({
+						filetypes = eslint_filetypes,
+					}),
+
+          -- spelling
+					cspell.code_actions,
+
+					-- general
+					none_ls.builtins.code_actions.gitsigns,
+				},
+			}
+		end,
+	},
 }
