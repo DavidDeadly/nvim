24a0a1be4550f542f2191b7d4d9c94669aa1d312
DavidDeadly <jdrueda513@gmail.com>
2023-11-05 01:33:33 -0500

    customize mini starter to restore sessions, change themes and launch common lazy action

--- lua/daviddeadly/plugins/starter.lua
+++ lua/daviddeadly/plugins/starter.lua
@@ -1,14 +1,72 @@
-return {
-  {
-    "echasnovski/mini.starter", version = "*",
-    opts = {}
-  },
+-- luacheck: globals vim
 
-  {
-    "dstein64/vim-startuptime",
-    cmd = "StartupTime",
-    init = function()
-      vim.g.startuptime_tries = 10
-    end,
-  },
+local function get_restore_session_item()
+	local session_info = require("projections.session").info(vim.loop.cwd() or "")
+	if type(session_info) == "table" then
+		return {
+			action = "RestoreProjectSession",
+			name = "R: Restore session",
+			section = "Sessions",
+		}
+	end
+end
+
+local function get_themes_items()
+	local themes = { "rose-pine", "nightfly", "tokyonight", "catppuccin", "onedark" }
+	local current_theme = vim.api.nvim_exec("colorscheme", true) or ""
+
+	themes = vim.tbl_filter(function(theme)
+		return not string.find(current_theme or "", theme)
+	end, themes)
+
+	local themes_items = {}
+	for i, theme in pairs(themes) do
+		local item = {
+			action = "colorscheme " .. theme,
+			name = i .. ": " .. theme,
+			section = "Themes",
+		}
+
+		table.insert(themes_items, item)
+	end
+
+	return themes_items
+end
+
+return {
+	{
+		"echasnovski/mini.starter",
+		version = "*",
+		opts = function()
+			local starter = require("mini.starter")
+
+			return {
+				content_hooks = {
+					starter.gen_hook.adding_bullet(""),
+					starter.gen_hook.aligning("center", "center"),
+				},
+				evaluate_single = true,
+				footer = os.date(),
+				header = table.concat({
+					[[Welcome back DavidDeadly to ]],
+					[[]],
+					[[  /\ \▔\___  ___/\   /(●)_ __ ___  ]],
+					[[ /  \/ / _ \/ _ \ \ / / | '_ ` _ \ ]],
+					[[/ /\  /  __/ (_) \ V /| | | | | | |]],
+					[[\_\ \/ \___|\___/ \_/ |_|_| |_| |_|]],
+					[[───────────────────────────────────]],
+				}, "\n"),
+				query_updaters = [[abcdefghilmoqrstuvwxyz0123456789_-,.ABCDEFGHIJKLMOQRSTUVWXYZ]],
+				items = {
+					{ action = "tab G", name = "G: Fugitive", section = "Git" },
+					get_restore_session_item(),
+					{ action = "Lazy update", name = "U: Update Plugins", section = "Plugins" },
+					{ action = "Lazy profile", name = "T: Lazy time", section = "Plugins" },
+					get_themes_items(),
+					{ action = "enew", name = "E: New Buffer", section = "Builtin actions" },
+					{ action = "qall!", name = "Q: Quit Neovim", section = "Builtin actions" },
+				},
+			}
+		end,
+	},
 }
