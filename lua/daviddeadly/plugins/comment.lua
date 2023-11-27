0284bcee2f0afcfb953f7abd74679fa9b7873152
DavidDeadly <jdrueda513@gmail.com>
2023-11-26 22:05:04 -0500

    fix: update ts-context-comment-string config due to deprecated module

--- lua/daviddeadly/plugins/comment.lua
+++ lua/daviddeadly/plugins/comment.lua
@@ -1,21 +1,31 @@
-return {
-  {
-    'numToStr/Comment.nvim',
-    dependencies = { "JoosepAlviste/nvim-ts-context-commentstring" },
-    keys = {
-      "gcc", "gc", "gbc"
-    },
-    config = function ()
-      require('Comment').setup {
-        pre_hook = require('ts_context_commentstring.integrations.comment_nvim').create_pre_hook(),
-      }
-    end
-  },
+-- luacheck: globals vim
+vim.g.skip_ts_context_commentstring_module = true
 
-  {
-    "folke/todo-comments.nvim",
-    cmd = { "TodoTelescope" },
-    config = true,
+return {
+	{
+		"numToStr/Comment.nvim",
+		dependencies = {
+			{
+				"JoosepAlviste/nvim-ts-context-commentstring",
+				main = "ts_context_commentstring",
+			},
+		},
+		keys = {
+			"gcc",
+			"gc",
+			"gbc",
+		},
+		config = function()
+			require("Comment").setup({
+				pre_hook = require("ts_context_commentstring.integrations.comment_nvim").create_pre_hook(),
+			})
+		end,
+	},
+
+	{
+		"folke/todo-comments.nvim",
+		cmd = { "TodoTelescope" },
+		config = true,
     -- stylua: ignore
     keys = {
       { "]t", function() require("todo-comments").jump_next() end, desc = "Next todo comment" },
@@ -23,5 +33,5 @@ return {
       { "<leader>st", "<cmd>TodoTelescope<cr>", desc = "Todo" },
       { "<leader>sT", "<cmd>TodoTelescope keywords=TODO,FIX,FIXME<cr>", desc = "Todo/Fix/Fixme" },
     },
-  }
+	},
 }
