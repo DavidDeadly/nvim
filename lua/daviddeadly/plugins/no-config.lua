574ed6b4a93a50fcd9ffed0e5907ce0a86caed1e
DavidDeadly <jdrueda513@gmail.com>
2023-12-05 00:38:47 -0500

    update & format

--- lua/daviddeadly/plugins/no-config.lua
+++ lua/daviddeadly/plugins/no-config.lua
@@ -1,49 +1,49 @@
 return {
-  {
-    "theprimeagen/vim-be-good",
-    cmd = "VimBeGood"
-  },
+	{
+		"theprimeagen/vim-be-good",
+		cmd = "VimBeGood",
+	},
 
-  {
-    "wakatime/vim-wakatime",
-  },
+	{
+		"wakatime/vim-wakatime",
+	},
 
-  {
-    "christoomey/vim-tmux-navigator",
-  },
+	{
+		"christoomey/vim-tmux-navigator",
+	},
 
-  {
-    "m4xshen/hardtime.nvim",
-    event = { "BufReadPost", "BufNewFile" },
-    dependencies = { "MunifTanjim/nui.nvim", "nvim-lua/plenary.nvim" },
-    opts = {
-      disabled_filetypes = {
-        "dapui_scopes",
-        "dapui_breakpoints",
-        "dapui_console",
-        "dapui_stacks",
-        "dapui_watches",
-        "dapui_repl",
-        "dap-float",
-        "FTerm",
-        "netrw",
-        "lazy",
-        "mason",
-        "minifiles",
-        "NvimTree"
-      },
-    }
-  },
+	{
+		"m4xshen/hardtime.nvim",
+		event = { "BufReadPost", "BufNewFile" },
+		dependencies = { "MunifTanjim/nui.nvim", "nvim-lua/plenary.nvim" },
+		opts = {
+			disabled_filetypes = {
+				"dapui_scopes",
+				"dapui_breakpoints",
+				"dapui_console",
+				"dapui_stacks",
+				"dapui_watches",
+				"dapui_repl",
+				"dap-float",
+				"FTerm",
+				"netrw",
+				"lazy",
+				"mason",
+				"minifiles",
+				"NvimTree",
+			},
+		},
+	},
 
-  {
-    "echasnovski/mini.move",
-    keys = {
-      { "<M-j>", mode = { "n", "v" } },
-      { "<M-k>", mode = { "n", "v" } },
-      { "<M-h>", mode = { "n", "v" } },
-      { "<M-l>", mode = { "n", "v" } },
-    },
-    version = "*",
-    opts = {}
-  },
+	{
+		"echasnovski/mini.move",
+		keys = {
+			{ "<M-j>", mode = { "n", "v" } },
+			{ "<M-k>", mode = { "n", "v" } },
+			{ "<M-h>", mode = { "n", "v" } },
+			{ "<M-l>", mode = { "n", "v" } },
+		},
+		version = "*",
+		opts = {},
+	},
 }
